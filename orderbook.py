d56bbe566bc37acb208758047c5247f50abd0e12
Pat <120336989+pszrk@users.noreply.github.com>
2023-10-10 15:55:42 -0400

    add orderbook output

--- orderbook.py
+++ orderbook.py
@@ -3,12 +3,16 @@ class Order:
         self.side = side
         self.price = price
         self.quantity = quantity
+    
+    def __str__(self):
+        return f"[{self.side};{self.price};{self.quantity}]."
 
 
 class OrderBook:
     def __init__(self):
-        self.bids = {}
+        self.bids = {}  # dictionary with key: price, value: [list of orders]
         self.asks = {}
+        self.price = 0
     
     def add_order(self, order):
         if order.side == 'buy':
@@ -42,6 +46,7 @@ class OrderBook:
         remaining_quantity = quantity
         while remaining_quantity > 0 and orders:
             order = orders.pop(0)
+            self.price = order.price
             if order.quantity <= remaining_quantity:
                 remaining_quantity -= order.quantity
             else:
@@ -49,22 +54,37 @@ class OrderBook:
                 remaining_quantity = 0
                 orders.insert(0, order)
     
+def print_book(book):
+    print("-------- Order Book: ------")
+    for ask in book.asks:
+        out = f"Sell Orders: {ask} "
+        for o in book.asks[ask]:
+            out += str(o)
+        print(out)
+    for bid in book.bids:
+        out = f"Buy Orders: {bid} "
+        for o in book.bids[bid]:
+            out += str(o)
+        print(out)
+    print("---------------------------")
 
 order_book = OrderBook()
 
 
 order_book.add_order(Order(side='buy', price=100.0, quantity=10))
 order_book.add_order(Order(side='buy', price=99.5, quantity=5))
+order_book.add_order(Order(side='buy', price=100.0, quantity=1))
 
 
 order_book.add_order(Order(side='sell', price=101.0, quantity=8))
 order_book.add_order(Order(side='sell', price=102.0, quantity=12))
 
 
-print("Buy Orders:", order_book.bids)
-print("Sell Orders:", order_book.asks)
+print_book(order_book)
 
-order_book.add_order(Order(side='buy', price=108.0, quantity=20))
+order_book.add_order(Order(side='buy', price=108.0, quantity=22))
 
-print("Buy Orders:", order_book.bids)
-print("Sell Orders:", order_book.asks)
+print_book(order_book)
+print("price:", order_book.price)
+print("highest bid:", max((o for o in order_book.bids), default='n/a'))
+print("lowest ask:", min((o for o in order_book.asks), default='n/a'))
