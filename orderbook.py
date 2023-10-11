927a95a1b486823811181dcdb85744456007ea54
Pat <120336989+pszrk@users.noreply.github.com>
2023-10-11 15:09:27 -0400

    add orderflow simulation

--- orderbook.py
+++ orderbook.py
@@ -1,3 +1,5 @@
+import json
+
 class Order:
     def __init__(self, side, price, quantity):
         self.side = side
@@ -6,6 +8,14 @@ class Order:
     
     def __str__(self):
         return f"[{self.side};{self.price};{self.quantity}]."
+    
+    def to_dict(self):
+        # Convert the Order object to a dictionary
+        return {
+            'side': self.side,
+            'price': self.price,
+            'quantity': self.quantity
+        }
 
 
 class OrderBook:
@@ -13,6 +23,16 @@ class OrderBook:
         self.bids = {}  # dictionary with key: price, value: [list of orders]
         self.asks = {}
         self.price = 0
+
+    def to_json_format(self):
+        bids_data = [[price, sum(order.quantity for order in orders)] for price, orders in self.bids.items()]
+        asks_data = [[price, sum(order.quantity for order in orders)] for price, orders in self.asks.items()]
+
+        return {
+            'price': self.price,
+            'bids': bids_data,
+            'asks': asks_data
+        }
     
     def add_order(self, order):
         if order.side == 'buy':
@@ -25,35 +45,53 @@ class OrderBook:
                 self.asks[order.price].append(order)
             else:
                 self.asks[order.price] = [order]
-        self.match_orders()
+        self.match_orders(order)
     
-    def match_orders(self):
+    def match_orders(self, newest_order):
         for buy_price in sorted(self.bids.keys(), reverse=True):
             for sell_price in sorted(self.asks.keys()):
                 if buy_price >= sell_price:
+                    print(f"trying to match buy{buy_price} to sell{sell_price}")
                     matched_quantity = min(
                         sum(o.quantity for o in self.bids[buy_price]),
                         sum(o.quantity for o in self.asks[sell_price])
                     )
+                    print(f" matched {matched_quantity}")
+                    self.price == sell_price
                     self.execute_orders(self.bids[buy_price], matched_quantity)
                     self.execute_orders(self.asks[sell_price], matched_quantity)
                     if not self.bids[buy_price]:
+                        print (f"removing bids key at {buy_price}")
                         del self.bids[buy_price]
                     if not self.asks[sell_price]:
+                        print (f"removing asks key at {sell_price}")
                         del self.asks[sell_price]
     
     def execute_orders(self, orders, quantity):
         remaining_quantity = quantity
         while remaining_quantity > 0 and orders:
             order = orders.pop(0)
-            self.price = order.price
             if order.quantity <= remaining_quantity:
                 remaining_quantity -= order.quantity
             else:
                 order.quantity -= remaining_quantity
                 remaining_quantity = 0
                 orders.insert(0, order)
+
+
+    def process_order_update(self, add_or_remove, order):
+        if add_or_remove == 'add':
+            self.add_order(order)
+        elif add_or_remove == 'remove':
+            self.remove_order(order)
+        return self
+
+
+    def get_status(self):
+        order_book_json = json.dumps(order_book.to_json_format(), indent=2)
+        return order_book_json
     
+
 def print_book(book):
     print("-------- Order Book: ------")
     for ask in book.asks:
@@ -71,20 +109,4 @@ def print_book(book):
 order_book = OrderBook()
 
 
-order_book.add_order(Order(side='buy', price=100.0, quantity=10))
-order_book.add_order(Order(side='buy', price=99.5, quantity=5))
-order_book.add_order(Order(side='buy', price=100.0, quantity=1))
-
-
-order_book.add_order(Order(side='sell', price=101.0, quantity=8))
-order_book.add_order(Order(side='sell', price=102.0, quantity=12))
-
-
 print_book(order_book)
-
-order_book.add_order(Order(side='buy', price=108.0, quantity=22))
-
-print_book(order_book)
-print("price:", order_book.price)
-print("highest bid:", max((o for o in order_book.bids), default='n/a'))
-print("lowest ask:", min((o for o in order_book.asks), default='n/a'))
---
+++ simulate_orders.py
@@ -0,0 +1,31 @@
+import time
+import random
+from orderbook import OrderBook, Order
+
+def generate_random_order():
+    side = random.choice(['buy', 'sell'])
+    price = round(random.uniform(45,50), 2)
+    quantity = random.randint(1, 10)
+    return Order(side, price, quantity)
+
+
+def simulate_order_flow(order_book, num_orders, delay):
+    for _ in range(num_orders):
+        order = generate_random_order()
+        print(f"Sending Order: {order.side} {order.quantity} @ {order.price}")
+        order_book.process_order_update('add', order)        
+        time.sleep(delay)
+
+
+def simulate_random_order(order_book, num_orders):
+    for _ in range(num_orders):
+        order = generate_random_order()
+        print(f"Sending Order: {order.side} {order.quantity} @ {order.price}")
+        return order_book.process_order_update('add', order)
+
+
+
+if __name__ == "__main__":
+    order_book = OrderBook()
+
+    simulate_order_flow(order_book, 10, 1)
