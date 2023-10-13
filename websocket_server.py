ce60f8ea49d02b3b9249066cf3317872d5e65eea
Pat <120336989+pszrk@users.noreply.github.com>
2023-10-13 13:09:02 -0400

    reorganize async code
    modify await statements to more efficiently await server and simulation, add handling for disconnecting sockets

--- websocket_server.py
+++ websocket_server.py
@@ -1,6 +1,7 @@
 import asyncio
 import json
 import websockets
+from websockets.exceptions import ConnectionClosedError
 from orderbook import OrderBook
 from simulate_orders import simulate_random_order
 
@@ -11,13 +12,25 @@ class WebSocketServer:
         self.server = None
 
 
-    async def order_book_server(self, websocket, path):
+    async def websocket_handler(self, websocket, path):
         self.websockets.add(websocket)
+        print(f"added websocket {websocket}")
         try:
             while True:
                 await asyncio.sleep(1)  # Keep the connection open
+        except ConnectionClosedError as e:
+            print(f"Client disconnected: {e}")
+        except Exception as e:
+            print(f"Error: {e}")
         finally:
             self.websockets.remove(websocket)
+
+
+    async def disconnect_all(self):
+        # disconnect all connected websockets
+        for websocket in self.websockets:
+            await websocket.close()
+        self.websockets.clear()
 
 
     async def send_order_book_update(self, data):
@@ -29,24 +42,29 @@ class WebSocketServer:
     
     async def start_server(self):
         self.server = await websockets.serve(
-            self.order_book_server,
-            "localhost",
+            self.websocket_handler,
+            "localhost", #0.0.0.0 = allow connections from any ip
             5000
         )
-        await self.server.wait_closed()
+        print("server running")
+        # await self.server.wait_closed() # this line ensures this method does not exit until server is closed
+
 
     async def stop_server(self):
         if self.server:
             print("trying to close server")
+            await self.disconnect_all()
             self.server.close()
             await self.server.wait_closed()
+            print("all websockets disconnected and server closed")
         else: print("no server found")
 
-async def run_simulation(server, delay):
+
+
+async def run_server_simulation(server, delay):
     for _ in range(100):
-        simulate_random_order(server.order_book, 1)
+        simulate_random_order(server.order_book)
         order_book_status = server.order_book.get_status()
-        #print(f"status is {order_book_status}")
         await server.send_order_book_update(order_book_status)
         await(asyncio.sleep(delay))        
 
@@ -55,13 +73,11 @@ async def main():
     orderbook = OrderBook()
     srv = WebSocketServer(orderbook)
     server_task = asyncio.create_task(srv.start_server())
-    simulation_task = asyncio.create_task(run_simulation(srv, 8))
+    simulation_task = asyncio.create_task(run_server_simulation(srv, 8))
     
+    await server_task
     await simulation_task
     await srv.stop_server()
-
-    # Use gather to await both tasks
-    await asyncio.gather(server_task, simulation_task)
 
 
 if __name__ == '__main__':
