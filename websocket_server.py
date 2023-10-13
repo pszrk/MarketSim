4329118d01ab503f8e4c18f7017e1e431f259e88
Pat <120336989+pszrk@users.noreply.github.com>
2023-10-13 18:02:16 -0400

    add custom order functionality
    add functions for receiving websocket messages, extracting order data from a websocket message, and submitting that as an order

--- websocket_server.py
+++ websocket_server.py
@@ -3,7 +3,7 @@ import json
 import websockets
 from websockets.exceptions import ConnectionClosedError
 from orderbook import OrderBook
-from simulate_orders import simulate_random_order
+from simulate_orders import simulate_random_order, send_custom_order
 
 class WebSocketServer:
     def __init__(self, order_book):
@@ -17,13 +17,67 @@ class WebSocketServer:
         print(f"added websocket {websocket}")
         try:
             while True:
-                await asyncio.sleep(1)  # Keep the connection open
+                message = await websocket.recv()
+                await self.process_received_message(message)
+                if message is None:
+                    break  # client has closed the connection
+                else:
+                    print(f"Received message from {websocket.remote_address}: {message}")
         except ConnectionClosedError as e:
             print(f"Client disconnected: {e}")
         except Exception as e:
             print(f"Error: {e}")
         finally:
             self.websockets.remove(websocket)
+
+
+    def extract_order_data(message):
+        try:
+            # Parse the JSON string into a Python dictionary
+            order_data = json.loads(message)
+
+            price = order_data['price']
+            amount = order_data['amount']
+            side = order_data['side']
+
+            return {
+                'price': price,
+                'amount': amount,
+                'side': side
+            }
+        except json.JSONDecodeError:
+            return None  # Return None if the message is not a valid JSON
+
+
+    async def process_received_message(self, message):
+        try:
+            data = json.loads(message)
+            # check if dictionary
+            if isinstance(data, dict):
+                if all(key in data for key in ['price', 'amount', 'side']):
+                    print("received message to submit custom order")
+                    await self.submit_custom_order(
+                        data['side'], int(data['price']), int(data['amount'])
+                    )
+                elif 'random' in data:
+                    print("received message to generate random order")
+                    await self.submit_random_order()
+            else:
+                print("unknown data format")
+        except json.JSONDecodeError:
+            print("message not in json format")
+
+    
+    async def submit_random_order(self):
+        simulate_random_order(self.order_book)
+        order_book_status = self.order_book.get_status()
+        await self.send_order_book_update(order_book_status)
+
+
+    async def submit_custom_order(self, side, price, quantity):
+        send_custom_order(self.order_book, side, price, quantity)
+        order_book_status = self.order_book.get_status()
+        await self.send_order_book_update(order_book_status)
 
 
     async def disconnect_all(self):
@@ -71,21 +125,27 @@ class WebSocketServer:
 
 
 async def run_server_simulation(server, delay):
-    for _ in range(3):
+    for _ in range(50):
         simulate_random_order(server.order_book)
         order_book_status = server.order_book.get_status()
         await server.send_order_book_update(order_book_status)
-        await(asyncio.sleep(delay))        
+        await(asyncio.sleep(delay))      
+
+
+async def run_server_length():
+    await(asyncio.sleep(600))
 
 
 async def main():
     orderbook = OrderBook()
     srv = WebSocketServer(orderbook)
     server_task = asyncio.create_task(srv.start_server())
-    simulation_task = asyncio.create_task(run_server_simulation(srv, 8))
+    #simulation_task = asyncio.create_task(run_server_simulation(srv, 8))
+    server_uptime_task = asyncio.create_task(run_server_length())
     
     await server_task
-    await simulation_task
+    #await simulation_task
+    await server_uptime_task
     await srv.stop_server()
 
 
