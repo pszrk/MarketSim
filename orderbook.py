87d2be0b177beee4d6d071b2bc74ef7cbbf9bee8
pszrk <120336989+pszrk@users.noreply.github.com>
2024-07-19 17:26:12 -0400

    refactor ordermatching engine
    refactor order matching algorithm to be more straightforward and efficient. all incoming orders are added to the orderbook, then match_orders() is called.

--- orderbook.py
+++ orderbook.py
@@ -1,26 +1,14 @@
-from collections import deque
-import copy
+
 
 class Order:
     def __init__(self, side, price, quantity):
         self.side = side
         self.price = price
         self.quantity = quantity
-    
-    def __str__(self):
-        return f"[{self.side};{self.price};{self.quantity}]."
-    
-    def to_dict(self):
-        return {
-            'side': self.side,
-            'price': self.price,
-            'quantity': self.quantity
-        }
-
 
 class OrderBook:
     def __init__(self):
-        self.bids = {} #dictionary with key:price, value:[deque of Order objects]
+        self.bids = {}  #empty dictionary
         self.asks = {}
         self.price = 0
         self.last_order = 'n/a'
@@ -28,178 +16,66 @@ class OrderBook:
         self.ask = 0
         self.filled_from_book = []
 
-    def to_json(self):
-        bids_data = {price: [order.to_dict() for order in orders] for price, orders in self.bids.items()}
-        asks_data = {price: [order.to_dict() for order in orders] for price, orders in self.asks.items()}
-        last_order = self.last_order
-        recent_fills = [o.to_dict() for o in self.filled_from_book]
-        return {
-            'price': self.price,
-            'bids': bids_data,
-            'asks': asks_data,
-            'last_order' : last_order,
-            'bid': self.bid,
-            'ask': self.ask,
-            'recent_fills' : recent_fills
-        }
+    def add_order(self, order):
+        if(order.side == 'bid'):
+            if(order.price) in self.bids:
+                self.bids[order.price] += order.quantity
+            else:
+                self.bids[order.price] = order.quantity
+        if(order.side == 'ask'):
+            if order.price in self.asks:
+                self.asks[order.price] += order.quantity
+            else:
+                self.asks[order.price] = order.quantity
     
-    def get_status(self):
-        return self.to_json()
-    
-    def add_order_to_book(self, order):
-        if order.side == 'buy':
-            if order.price not in self.bids:
-                self.bids[order.price] = deque()
-            self.bids[order.price].append(order)
-        elif order.side == 'sell':
-            if order.price not in self.asks:
-                self.asks[order.price] = deque()
-            self.asks[order.price].append(order)
+    def match_orders(self):
+        while self.bids and self.asks:
+            best_bid_price = max(self.bids)     #  highest key in self.bids dictionary
+            best_ask_price = min(self.asks)     #  lowest key in self.asks dictionary
+            
+            if best_bid_price >= best_ask_price:    
+                # Match orders
+                best_bid_order_qty = self.bids[best_bid_price]     # key = best bid price; value = quantity associated with that key in the bids dictionary
+                best_ask_order_qty = self.asks[best_ask_price]     # key = best ask price; value = quantity associated with that key in the asks dictionary
+                
+                matched_quantity = min(best_bid_order_qty, best_ask_order_qty)
+                print(f"Matched {matched_quantity} @ {best_ask_price}")
+                        
+                self.bids[best_bid_price] -= matched_quantity    # adjust the value associated with best_bid_price key
+                self.asks[best_ask_price] -= matched_quantity    # adjust the value associated with best_ask_price key
+                        
+                if self.bids[best_bid_price] == 0:      #  the value associated with best_bid_price key is zero
+                    del self.bids[best_bid_price]           #  delete best_bid_price key from dictionary              
+                if self.asks[best_ask_price] == 0:      #  the value associated with best_ask_price key is zero
+                    del self.asks[best_ask_price]           #  delete best_ask_price key from dictionary
 
+            else:  #  best bid price is not above or equal best ask price
+                break
 
-    def calculate_bid_ask(self):
-        if len(self.bids) > 0:
-            self.bid = max(self.bids)
-        else:
-            self.bid = 0
-        if len(self.asks) > 0:
-            self.ask = min(self.asks)
-        else:
-            self.ask = 0
-    
-
-    def buy_order(self, order):
-        if self.asks and order.price >= min(self.asks): # buy order is above or equal to lowest ask price
-            self.execute_buy(order)
-        else:
-            self.add_order_to_book(order)
-
-    def sell_order(self, order):
-        if self.bids and order.price <= max(self.bids): # sell order is below or equal to highest bid price
-            self.execute_sell(order)
-        else:
-            self.add_order_to_book(order)
-
-    def execute_buy(self, buyorder):
-        print(f"executing buy at {buyorder.price} qty {buyorder.quantity}")
-        while(buyorder.quantity > 0 and self.asks):
-            if(min(self.asks) <= buyorder.price): # lowest ask is less or equal to the buy order price
-                self.price = min(self.asks) # self.asks item is a dict with key price, value deque of Order objects
-                print(f"self.price is {self.price}")
-                sell_orders_at_price = self.asks[self.price] # reference deque of all sell orders at this price
-                front_sell_order_at_price = sell_orders_at_price.popleft() # get the first/oldest sell order at this price(from front of deque)
-                print(f"first sell order at price {front_sell_order_at_price.price} qty {front_sell_order_at_price.quantity}")
-                if front_sell_order_at_price.quantity < buyorder.quantity:
-                    # this sell order is not enough to fill the entire buy order, 
-                    # we do nothing more to the sell order since it is already popped from the deque,
-                    self.filled_from_book.append(front_sell_order_at_price) # just add the sell order to current list of filled asks
-                    # we decrement buyorder quantity by the contracts of the sell order
-                    print(f"that is not enough to fill the whole buy order")
-                    buyorder.quantity -= front_sell_order_at_price.quantity
-                    print(f"buyorder qty is now {buyorder.quantity}")
-                    if not self.asks[self.price]:  # also remove the price entry from dict if its deque of orders is empty
-                        del self.asks[self.price]
-                        print(f"deleted self.asks entry at {self.price}")
-                else: # current sell order is enough to fill the entire buy order
-                    print(f"it is enough to fill the entire buy order")    
-                    if front_sell_order_at_price.quantity >= buyorder.quantity:
-                        #the sell order on the book got partially filled, and we want to add the order that got partially filled to list of fills for this incoming buy order. 
-                        partialfill = copy.copy(front_sell_order_at_price)
-                        partialfill.quantity = buyorder.quantity
-                        self.filled_from_book.append(partialfill)               
-                    front_sell_order_at_price.quantity -= buyorder.quantity                    
-                    buyorder.quantity = 0
-                    # remove buy order bid price key from dict as well, if it was added earlier due to a partial fill.
-                    if buyorder.price in self.bids and not self.bids[buyorder.price]:
-                        del self.bids[buyorder.price]
-                        print(f"removed buyorder bid key {buyorder.price} from dict")
-                    print(f"remaining sell order qty {front_sell_order_at_price.quantity}")
-                    if front_sell_order_at_price.quantity > 0: #this particular sell order still has contracts remaining unfilled, so add it back to the front of the deque of sell orders at its price
-                        sell_orders_at_price.appendleft(front_sell_order_at_price)
-                        print(f"appending remaing sell order back to the orders at this price.")
-                    elif not self.asks[self.price]:
-                        del self.asks[self.price]
-                        print(f"sell order fully filled and no remaining asks at {self.price}, removing price from dict")
-            else: #lowest ask is not less or equal to the buy price, so add the buy order to list of bids and return.
-                self.add_order_to_book(buyorder)
-                print(f"adding buy order to book, no asks sufficient to fill it.")
-                return # return here to avoid double posting the order to the book
-        if(buyorder.quantity > 0): #lowest ask is not less or equal to the buy price, so add the buy order to list of bids and return.
-                self.add_order_to_book(buyorder)
-                print(f"adding buy order to book, no asks sufficient to fill it.")
+    def display_orderbook(self):
+        print("BIDS:")
+        for key, value in self.bids.items():
+            print(f"BID Price: {key}, Qty: {value}")
         
-
-    def execute_sell(self, sellorder):
-        print(f"executing sell at {sellorder.price} qty {sellorder.quantity}")
-        while(sellorder.quantity > 0 and self.bids):
-            if(max(self.bids) >= sellorder.price): # highest bid is above or equal to the sell order price
-                self.price = max(self.bids) # self.bids item is a dict with key price, value deque of Order objects
-                print(f"self.price is {self.price}")
-                buy_orders_at_price = self.bids[self.price] # reference deque of all buy orders at this price
-                front_buy_order_at_price = buy_orders_at_price.popleft() # get the first/oldest buy order at this price
-                print(f"first buy order at price {front_buy_order_at_price.price} qty {front_buy_order_at_price.quantity}")
-                if front_buy_order_at_price.quantity < sellorder.quantity:
-                    # this buy order is not enough to fill the entire sell order, 
-                    # we do nothing more to the buy order since it is already popped from the deque,
-                    self.filled_from_book.append(front_buy_order_at_price) # just add the buy order to current list of filled bids
-                    # we decrement sellorder quantity by the contracts of the buy order
-                    print(f"that is not enough to fill the whole sell order")
-                    sellorder.quantity -= front_buy_order_at_price.quantity
-                    print(f"sellorder qty is now {sellorder.quantity}")
-                    if not self.bids[self.price]:  # also remove the price entry from dict if its deque of orders is empty
-                        del self.bids[self.price]
-                        print(f"deleted self.bids entry at {self.price}")
-                else: # current buy order is enough to fill the entire sell order
-                    print(f"it is enough to fill the entire sell order")
-                    if front_buy_order_at_price.quantity >= sellorder.quantity:
-                        # the sell order on the book got partially filled, and we want to add the order that got partially filled to list of fills for this incoming sell order. 
-                        partialfill = copy.copy(front_buy_order_at_price)
-                        partialfill.quantity = sellorder.quantity
-                        self.filled_from_book.append(partialfill)         
-                    front_buy_order_at_price.quantity -= sellorder.quantity
-                    sellorder.quantity = 0
-                    # remove sell order ask price key from dict as well, if it was added earlier due to a partial fill.
-                    if sellorder.price in self.asks and not self.asks[sellorder.price]:
-                        del self.asks[sellorder.price]
-                        print(f"removed sellorder ask key {sellorder.price} from dict")
-                    print(f"remaining buy order qty {front_buy_order_at_price.quantity}")
-                    if front_buy_order_at_price.quantity > 0: #this particular buy order still has contracts remaining unfilled, so add it back to the front of the deque of buy orders at its price
-                        buy_orders_at_price.appendleft(front_buy_order_at_price)
-                        print(f"appending remaing buy order back to the orders at this price.")
-                    elif not self.bids[self.price]:
-                        del self.bids[self.price]
-                        print(f"buy order fully filled and no remaining bids at {self.price}, removing price from dict")
-            else: #highest bid is not above or equal to the sell price, so add the sell order to list of asks and return.
-                self.add_order_to_book(sellorder)
-                print(f"adding sell order to book, no asks sufficient to fill it.")
-                return # return here to avoid double posting the order to the book
-        if(sellorder.quantity > 0): #highest bid is not above or equal to the sell price, so add the sell order to list of asks and return.
-                self.add_order_to_book(sellorder)
-                print(f"adding sell order to book, no asks sufficient to fill it.")
-        
-
-
-    def process_order_update(self, add_or_remove, order):
-        try:
-            if order.side not in {'buy', 'sell'}: return
-            if not int (order.quantity): return
-            if not float(order.price): return
-        except Exception as e:
-            return
-
-        self.filled_from_book.clear()
-        self.last_order = f"{order.side} {order.quantity} @ {order.price:.2f}"
-        if add_or_remove == 'add':
-            if(order.side == 'buy'):
-                self.buy_order(order)
-            elif (order.side == 'sell'):
-                self.sell_order(order)
-        self.calculate_bid_ask()
+        print("\nASKS:")
+        for key, value in self.asks.items():
+            print(f"ASK Price: {key}, Qty: {value}")
 
 
 if __name__ == "__main__":
     order_book = OrderBook()
-    print("running orderbook.py as main")
-    order_book.process_order_update('add', Order('buy', 46.45, 3))
-    order_book.process_order_update('add', Order('buy', 48.45, 9))
-    order_book.process_order_update('add', Order('sell', 45.71, 3))
+
+    order_book.add_order(Order('bid', 100, 10))
+    order_book.add_order(Order('bid', 99, 5))
+    order_book.add_order(Order('bid', 98, 8))
+
+    order_book.add_order(Order('ask', 99, 25))
+    order_book.add_order(Order('ask', 103, 7))
+
+    print("Initial Orderbook State:")
+    order_book.display_orderbook()
+
+    order_book.match_orders()
+
+    print("\nUpdated Orderbook State:")
+    order_book.display_orderbook()
--- server.py
+++ server.py
@@ -1,159 +1,37 @@
-from orderbook import OrderBook
-from simulate_orders import simulate_random_order, send_custom_order
+from flask import Flask, request, jsonify
 
-class WebSocketServer:
-    def __init__(self, order_book):
-        self.order_book = order_book
-        self.websockets = set()
-        self.server = None
+app = Flask(__name__)
 
+#  receives post request with price/qty/side values, validates request data, and calls process_order()
+@app.route("/submit/", methods =["POST"])
+def receive_order():
+    data = request.get_json()
 
-    async def websocket_handler(self, websocket, path):
-        self.websockets.add(websocket)
-        print(f"added websocket {websocket}")
-        try:
-            while True:
-                message = await websocket.recv()
-                await self.process_received_message(message)
-                if message is None:
-                    break  # client has closed the connection
-                else:
-                    print(f"Received message from {websocket.remote_address}: {message}")
-        except ConnectionClosedError as e:
-            print(f"Client disconnected: {e}")
-        except Exception as e:
-            print(f"Error: {e}")
-        finally:
-            self.websockets.remove(websocket)
-
-
-    def extract_order_data(message):
-        try:
-            # Parse the JSON string into a Python dictionary
-            order_data = json.loads(message)
-
-            price = order_data['price']
-            amount = order_data['amount']
-            side = order_data['side']
-
-            return {
-                'price': price,
-                'amount': amount,
-                'side': side
-            }
-        except json.JSONDecodeError:
-            return None  # Return None if the message is not a valid JSON
-
-
-    async def process_received_message(self, message):
-        try:
-            data = json.loads(message)
-            # check if dictionary
-            if isinstance(data, dict):
-                if all(key in data and data[key] for key in ['price', 'amount', 'side']):
-                    print("received message to submit custom order")
-                    await self.submit_custom_order(
-                        data['side'], int(data['price']), int(data['amount'])
-                    )
-                elif 'random' in data:
-                    print("received message to generate random order")
-                    await self.submit_random_order()
-            else:
-                print("unknown data format")
-        except json.JSONDecodeError:
-            print("message not in json format")
-
+    if "price" not in data or data["price"] =="":
+        return jsonify({"error": "Missing price parameter"}), 400   
+    if "qty" not in data or data["qty"] =="":
+        return jsonify({"error": "Missing quantity parameter"}), 400
+    if "side" not in data or data["side"] not in ["bid", "ask"]:
+        return jsonify({"error": "Missing side parameter"}), 400
     
-    async def submit_random_order(self):
-        simulate_random_order(self.order_book)
-        order_book_status = self.order_book.get_status()
-        await self.send_order_book_update(order_book_status)
-
-
-    async def submit_custom_order(self, side, price, quantity):
-        try:
-            if side not in {'buy', 'sell'} or \
-                not int(quantity) or \
-                not float(price):
-                        print("incorrect data format in submit_custom_order, order discarded.") 
-                        return
-        except Exception as e:
-            print("data format exception handled in submit_custom_order, order discarded.") 
-            return
-        
-        send_custom_order(self.order_book, side, price, quantity)
-        order_book_status = self.order_book.get_status()
-        await self.send_order_book_update(order_book_status)
-
-
-    async def disconnect_all(self):
-        # disconnect all connected websockets
-        for websocket in self.websockets:
-            try:
-                await websocket.close()
-            except Exception as e:
-                print(f"Error closing websocket: {e}")
-        self.websockets.clear()
-
-
-    async def send_order_book_update(self, data):
-        message = json.dumps(data)
-        #print(f"Sending message to client: {message}")
-        # Use asyncio.gather to send the message to all connected clients concurrently
-        asyncio.gather(*(websocket.send(message) for websocket in self.websockets))
-
+    #  price and quantity data validation:
+    try:
+        price = float(data["price"])
+    except ValueError:
+        return jsonify({"error": "Invalid price format, must be a number"}), 400
+    if price <= 0:
+        return jsonify({"error": "Price must be a positive number"}), 400    
+    try:
+        qty = int(data["price"])
+    except ValueError:
+        return jsonify({"error": "Invalid quantity format, must be an integer"}), 400
+    if qty <= 0:
+        return jsonify({"error": "Quantity must be a positive number"}), 400
     
-    async def start_server(self):
-        self.server = await websockets.serve(
-            self.websocket_handler,
-            "localhost", #0.0.0.0 = allow connections from any ip
-            5000
-        )
-        print("server running")
 
+    price = data["price"]
+    qty = data["qty"]
+    side = data["side"]
 
-    async def stop_server(self):        
-        if self.server:
-            print("trying to close server")
-            try:
-                await self.disconnect_all()
-                print("websockets disconnected")
-                if(self.server):
-                    self.server.close()
-                    await self.server.wait_closed()
-                else:
-                    print("not self server")
-                print("all websockets disconnected and server closed")
-            except Exception as e:
-                print(f"an error occured during server close: {e}")
-        else: print("no server found")
+    process_order(price, qty, side)
 
-
-
-async def run_server_simulation(server, delay):
-    for _ in range(50):
-        simulate_random_order(server.order_book)
-        order_book_status = server.order_book.get_status()
-        await server.send_order_book_update(order_book_status)
-        await(asyncio.sleep(delay))      
-
-
-async def run_server_length():
-    await(asyncio.sleep(600))
-
-
-async def main():
-    orderbook = OrderBook()
-    srv = WebSocketServer(orderbook)
-    server_task = asyncio.create_task(srv.start_server())
-    #simulation_task = asyncio.create_task(run_server_simulation(srv, 8))
-    server_uptime_task = asyncio.create_task(run_server_length())
-    
-    await server_task
-    #await simulation_task
-    await server_uptime_task
-    await srv.stop_server()
-
-
-if __name__ == '__main__':
-    asyncio.run(main()) 
